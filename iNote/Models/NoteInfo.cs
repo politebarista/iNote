835d6497157a602c96a24650a34de8bd9354100f
Sulimenko Ilya <smotri100123@gmail.com>
2020-11-09 06:42:37 +0700

    Добавьте файлы проекта.

---
+++ iNote/Models/NoteInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iNote.Models
+{
+    public class NoteInfo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public byte Color { get; set; }
+        public string LastChange { get; set; }
+    }
+}
