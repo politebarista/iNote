47b7ca0c1bde72e97ef8ae2ef83d9dcb7b3d56e4
Sulimenko Ilya <smotri100123@gmail.com>
2020-11-16 16:40:40 +0700

    реализована проверка старых данных с новыми

--- iNote/wwwroot/js/site.js
+++ iNote/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+back.onclick = function () {
+    console.log("работает");
+    if (document.getElementById("oldTitle").value != document.getElementById("newTitle").value || document.getElementById("oldDesc").value != document.getElementById("newDesc").value) {
+        console.log("отличаются");
+    } else {
+        console.log("идентичны");
+    }
+}
